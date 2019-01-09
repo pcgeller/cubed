ca65ddec9a6d5beb527438e16c160bfbabc47e95
pcgeller <pcgeller@gmail.com>
2019-01-08 19:13:38 -0500

    working out the block generation

--- solver.py
+++ solver.py
@@ -8,21 +8,27 @@ class block:
         self.previousOrientations = ''
         self.validOrientations = ''
         self.validPositions = ''
-    
+        self.order = ''
+
+    def __repr__(self):
+        return '{}  {}'.format(self.__class__.__name__, self.order)
 
 def initBlocks(cubeSequence):
-    for block in cubeSequence:
-        #set orientation
+    listOfBlocks = []
+    i = 0
+    for blockOrient in cubeSequence:
+        newBlock = block(blockOrient)
+        newBlock.order = i
+        listOfBlocks.append(newBlock)
+        i += 1
     return listOfBlocks
 
-initPosition = [0,0,0]
-maxPosition = [3,3,3]
-
 dimensionRange = range(0,3 + 1)
-
+orientation []
 allPossiblePoints = [(i,j,k) for i in dimensionRange for j in dimensionRange for k in dimensionRange]
-
+i = 0
 for startPoint in allPossiblePoints:
+    listOfBlocks[i].orientation
     #set orientation for initial block
     #validity check
     #for block in cubeSequence
